976bc1c470839b27a6c99149f977ba11ddcfb54e
Mike Busch <mbusch@heroku.com>
2021-12-03 13:44:50 -0500

    refactor to use bit shifting!

--- day-03/day.rb
+++ day-03/day.rb
@@ -9,49 +9,56 @@ end
 
 def parse_input(input)
   input.split("\n").map do |line|
-    line.chars.map(&:to_i)
+    line.to_i(2)
   end
 end
 
 ### CODE HERE ###
 
+def count_ones_and_zeros(readings, n)
+  tally = readings.map { |r| (r >> n) & 1 }.tally
+  [tally[0] || 0, tally[1] || 0]
+end
+
+def nth_bit_eq?(reading, n, x)
+  (reading >> n) & 1 == x
+end
+
 def grate(input)
-  first, *rest = input
-  digits = first.zip(*rest)
-  gamma = digits.map do |d|
-    d.count(1) >= d.length/2 ? 1 : 0
+  width = input.max.to_s(2).length
+  (1..width).reduce(0) do |gamma, n|
+    zeros, ones = count_ones_and_zeros(input, width - n)
+    ones >= zeros ? (gamma << 1) | 1 : gamma << 1
   end
-  gamma.join.to_i(2)
 end
 
 def erate(input)
-  first, *rest = input
-  digits = first.zip(*rest)
-  gamma = digits.map do |d|
-    d.count(1) < d.length/2 ? 1 : 0
+  width = input.max.to_s(2).length
+  (1..width).reduce(0) do |gamma, n|
+    zeros, ones = count_ones_and_zeros(input, width - n)
+    zeros >= ones ? (gamma << 1) | 1 : gamma << 1
   end
-  gamma.join.to_i(2)
 end
 
 def orate(input)
-  readings = input.dup
-  i = 0
-  while true
-    d = readings.count { |digits| digits[i] == 1 } * 2 >= readings.length ? 1 : 0
-    readings.select! { |digits| digits[i] == d }
-    return readings.first.join.to_i(2) if readings.length == 1
-    i += 1
+  width = input.max.to_s(2).length
+  (1..width).each_with_object(input.dup) do |n, readings|
+    zeros, ones = count_ones_and_zeros(readings, width - n)
+    readings.select! do |reading|
+      nth_bit_eq?(reading, width - n, ones >= zeros ? 1 : 0)
+    end
+    return readings.first if readings.length == 1
   end
 end
 
 def co2srate(input)
-  readings = input.dup
-  i = 0
-  while true
-    d = readings.count { |digits| digits[i] == 0 } * 2 <= readings.length ? 0 : 1
-    readings.select! { |digits| digits[i] == d }
-    return readings.first.join.to_i(2) if readings.length == 1
-    i += 1
+  width = input.max.to_s(2).length
+  (1..width).each_with_object(input.dup) do |n, readings|
+    zeros, ones = count_ones_and_zeros(readings, width - n)
+    readings.select! do |reading|
+      nth_bit_eq?(reading, width - n, ones < zeros ? 1 : 0)
+    end
+    return readings.first if readings.length == 1
   end
 end
 
