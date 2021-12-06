b511a45c77762644aa9bd40cff7864631a2edee4
Mike Busch <mbusch@heroku.com>
2021-12-06 13:22:08 -0500

    template: restructure ruby day file and improve makefile

--- template/day.rb
+++ template/day.rb
@@ -1,3 +1,9 @@
+def run
+  input = parse_input(read_input)
+
+  # code to run part 1 and part 2
+end
+
 def read_input(filename = "input.txt")
   if !STDIN.tty?
     ARGF.read
@@ -8,16 +14,31 @@ def read_input(filename = "input.txt")
 end
 
 def parse_input(input)
-  input.split("\n").map do |line|
-    Integer(line)
-  end
+  input.split(",").map(&:to_i).tally
 end
-
 
 ### CODE HERE ###
 
-return unless $PROGRAM_NAME == __FILE__ || $PROGRAM_NAME.end_with?("ruby-memory-profiler")
 
-input = parse_input(read_input)
+### TESTS HERE ###
+require "rspec"
 
-### RUN STUFF HERE ###
+describe "day" do
+  let(:input) do
+    parse_input(File.read("test.txt"))
+  end
+
+  let(:actual_input) do
+    parse_input(File.read("input.txt"))
+  end
+
+  it "should solve part 1" do
+    expect(true).to eq false
+  end
+
+  it "should solve part 2" do
+    expect(true).to eq false
+  end
+end
+
+run if $PROGRAM_NAME == __FILE__ || $PROGRAM_NAME.end_with?("ruby-memory-profiler")
