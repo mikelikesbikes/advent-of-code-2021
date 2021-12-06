29528bdef8c2dd3a5479c3d3b454fd5f674471e8
Mike Busch <mbusch@heroku.com>
2021-12-06 09:22:31 -0500

    fix spec

--- day-06/day_spec.rb
+++ day-06/day_spec.rb
@@ -10,10 +10,12 @@ describe "day" do
     parse_input(File.read("input.txt"))
   end
 
-  it "should ..." do
-    p input
+  it "should evolve the input n times" do
     expect(evolve(input, 18)).to eq 26
     expect(evolve(input, 80)).to eq 5934
     expect(evolve(input, 256)).to eq 26984457539
+
+    expect(evolve(actual_input, 80)).to eq 362346
+    expect(evolve(actual_input, 256)).to eq 1639643057051
   end
 end
