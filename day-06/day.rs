184f1b6f45f625167d6ab88e1ba904efc69d2685
Mike Busch <mbusch@heroku.com>
2021-12-06 09:22:43 -0500

    day 6 rust

--- day-06/day.rs
+++ day-06/day.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::HashMap;
 //use std::str::FromStr;
 //use std::convert::Infallible;
 
@@ -14,9 +15,29 @@ fn main() {
     let file = File::open(filename).expect("input file to be found");
     let input = &parse_input(read_input(file));
 
-    println!("input length: {}", input.len());
-    println!("{}", "part1");
-    println!("{}", "part2");
+    println!("{:?}", &input);
+    println!("{}", evolve(input, 80));
+    println!("{}", evolve(input, 256));
+}
+
+fn evolve(counts: &HashMap<usize, usize>, count: usize) -> usize {
+    let mut c = counts.clone();
+    for _ in 0..count {
+        let mut new_counts = HashMap::new();
+        for k in c {
+            match k {
+                (0, v) => {
+                    *new_counts.entry(8).or_insert(0) += v;
+                    *new_counts.entry(6).or_insert(0) += v;
+                },
+                (k, v) => {
+                    *new_counts.entry(k - 1).or_insert(0) += v;
+                },
+            }
+        }
+        c = new_counts;
+    }
+    c.values().sum()
 }
 
 fn read_input(file: File) -> Vec<String> {
@@ -24,24 +45,28 @@ fn read_input(file: File) -> Vec<String> {
     reader.lines().map(|l| l.unwrap()).collect()
 }
 
-fn parse_input(input: Vec<String>) -> Vec<String> {
-    input.iter().map(|l| {
-        // replace 'String' with the type we're trying to parse
-        l.parse::<String>().unwrap()
-    }).collect()
+fn parse_input(input: Vec<String>) -> HashMap<usize, usize> {
+    let mut vals = HashMap::new();
+    for s in input[0].split(",") {
+        let k = s.parse::<usize>().unwrap();
+        let count = vals.entry(k).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", &vals);
+    vals
 }
 
 #[cfg(test)]
 mod aoc_tests {
     use super::*;
 
-    fn test_input() -> Vec<String> {
+    fn test_input() -> HashMap<usize, usize> {
         let filename = "test.txt";
         let file = File::open(filename).expect("input file to be found");
         parse_input(read_input(file))
     }
 
-    fn actual_input() -> Vec<String> {
+    fn actual_input() -> HashMap<usize, usize> {
         let filename = "input.txt";
         let file = File::open(filename).expect("input file to be found");
         parse_input(read_input(file))
@@ -49,13 +74,14 @@ mod aoc_tests {
 
     #[test]
     fn test_part1() {
-        assert!(test_input().len() > 0);
-        assert!(actual_input().len() > 0);
-        assert_eq!(true, true);
+        assert_eq!(evolve(&test_input(), 18), 26);
+        assert_eq!(evolve(&test_input(), 80), 5934);
+        assert_eq!(evolve(&actual_input(), 80), 362346);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(true, true);
+        assert_eq!(evolve(&test_input(), 256), 26984457539);
+        assert_eq!(evolve(&actual_input(), 256), 1639643057051);
     }
 }
