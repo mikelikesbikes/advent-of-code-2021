f77645cd29508a3c00028414ed3f292188d15452
Mike Busch <mbusch@heroku.com>
2021-12-14 07:23:28 -0500

    use strings instead of char arrays

--- day-14/day.rb
+++ day-14/day.rb
@@ -27,10 +27,10 @@ Polymer = Struct.new(:pairs, :rules) do
     template, rules = str.split("\n\n")
     # delimit the string with a character so that we can track the last
     # character as the first character in the last pair
-    pairs = (template + "-").chars.each_cons(2).tally
+    pairs = (template + "-").chars.each_cons(2).map(&:join).tally
     rules = rules.split("\n").each_with_object({}) do |line, rules|
       pair, insert = line.split(" -> ")
-      rules[pair.chars] = insert
+      rules[pair] = insert
     end
     new(pairs, rules)
   end
@@ -38,8 +38,8 @@ Polymer = Struct.new(:pairs, :rules) do
   def expand
     self.pairs = pairs.each_with_object(Hash.new(0)) do |(k, v), pairs|
       if ch = rules[k]
-        pairs[[k[0], ch]] += v
-        pairs[[ch, k[1]]] += v
+        pairs[k[0] + ch] += v
+        pairs[ch + k[1]] += v
       else
         pairs[k] += v
       end
@@ -47,8 +47,8 @@ Polymer = Struct.new(:pairs, :rules) do
   end
 
   def score
-    tally = pairs.each_with_object(Hash.new(0)) do |((c1, _), v), tally|
-      tally[c1] += v
+    tally = pairs.each_with_object(Hash.new(0)) do |(k, v), tally|
+      tally[k[0]] += v
     end
     min, max = tally.values.minmax
     max - min
